750e87b455e521da66c7639461ccccc858a10aef
Marc Hulterström <marc.hulterstrom@gmail.com>
2021-02-12 10:11:01 +0100

    Setting up project and adding app icon

---
+++ Movieskip/Movieskip/Controller/HomeController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeController.swift
+//  Movieskip
+//
+//  Created by marchelmon on 2021-02-12.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+    }
+    
+}
