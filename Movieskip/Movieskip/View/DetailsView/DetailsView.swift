5a5cbc5b88cf5c2c6d1fda1ccdaaa9ad2bc9164d
Marc Hulterström <marc.hulterstrom@gmail.com>
2021-03-18 10:38:31 +0100

    Fixing poster not displaying in details and cleaning up details view code

--- Movieskip/Movieskip/View/DetailsView/DetailsView.swift
+++ Movieskip/Movieskip/View/DetailsView/DetailsView.swift
@@ -24,9 +24,9 @@ class DetailsView: UIView {
         return iv
     }()
 
-    lazy var rating: UIButton = {
+    private let rating: UIButton = {
         let rating = UIButton()
-        rating.setTitle("  \(movie.rating)/10", for: .normal)
+        
         rating.titleLabel?.font = UIFont.systemFont(ofSize: 25)
         rating.setTitleColor(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), for: .normal)
         rating.setImage(UIImage(systemName: "star.fill")?.withTintColor(#colorLiteral(red: 0.9529411793, green: 0.8256965162, blue: 0.1009962625, alpha: 1), renderingMode: .alwaysOriginal), for: .normal)
@@ -34,15 +34,14 @@ class DetailsView: UIView {
         return rating
     }()
     
-    lazy var releaseLabel: UILabel = {
+    private let releaseLabel: UILabel = {
         let label = UILabel()
-        label.text = "\(self.movie.released)"
         label.font = UIFont.systemFont(ofSize: 16)
         label.textColor = .gray
         return label
     }()
     
-    let trailerButton: UIButton = {
+    private let trailerButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Watch trailer", for: .normal)
         button.tintColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
@@ -54,7 +53,7 @@ class DetailsView: UIView {
         return button
     } ()
     
-    let genresLabel: UILabel = {
+    private let genresLabel: UILabel = {
         let label = UILabel()
         label.text = ""
         label.textColor = .lightGray
@@ -63,23 +62,21 @@ class DetailsView: UIView {
         return label
     }()
     
-    lazy var titleLabel: UILabel = {
+    private let titleLabel: UILabel = {
         let label = UILabel()
-        label.text = movie.title
         label.font = UIFont.systemFont(ofSize: 22, weight: .semibold)
         label.numberOfLines = 2
         label.textAlignment = .center
         return label
     }()
     
-    lazy var overviewText: UILabel = {
+    private let overviewText: UILabel = {
         let label = UILabel()
-        label.text = movie.overview
         label.numberOfLines = 8
         return label
     }()
     
-    let actorsLabel: UILabel = {
+    private let actorsLabel: UILabel = {
         let label = UILabel()
         label.text = "Actors"
         label.font = UIFont.boldSystemFont(ofSize: 18)
@@ -87,7 +84,7 @@ class DetailsView: UIView {
         return label
     }()
     
-    let actorsList: UILabel = {
+    private let actorsList: UILabel = {
         let label = UILabel()
         label.text = ""
         label.textColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
@@ -102,62 +99,12 @@ class DetailsView: UIView {
         self.movie = movie
         super.init(frame: .zero)
         backgroundColor = .systemGroupedBackground
-        
         frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
-
-        addSubview(posterImage)
-        posterImage.anchor(top: safeAreaLayoutGuide.topAnchor, left: leftAnchor, paddingTop: 15, paddingLeft: 15)
-        posterImage.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.45).isActive = true
-        posterImage.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 0.45 * 1.5).isActive = true
         
         
-        addSubview(rating)
-        addSubview(releaseLabel)
-        addSubview(trailerButton)
-        
-        rating.anchor(top: safeAreaLayoutGuide.topAnchor, left: posterImage.rightAnchor, paddingTop: 18, paddingLeft: 10)
-        releaseLabel.anchor(top: rating.bottomAnchor, left: posterImage.rightAnchor, paddingTop: 4, paddingLeft: 10)
-        trailerButton.anchor(top: releaseLabel.bottomAnchor, left: posterImage.rightAnchor, right: rightAnchor, paddingTop: 8, paddingLeft: 10, paddingRight: 15)
-        
-                
-        if let posterUrl = URL(string: "\(TMDB_IMAGE_BASE)\(movie.posterPath!)") {
-            posterImage.sd_setImage(with: posterUrl)
-        }
-
-        addSubview(genresLabel)
-
-        genresLabel.anchor(top: trailerButton.bottomAnchor, left: posterImage.rightAnchor, right: rightAnchor, paddingTop: 5, paddingLeft: 10, paddingRight: 5)
-
-        addSubview(titleLabel)
-        addSubview(overviewText)
-        
-        
-        titleLabel.anchor(top: posterImage.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 10, paddingLeft: 20, paddingRight: 20)
-        overviewText.anchor(top: titleLabel.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 10, paddingLeft: 20, paddingRight: 20)
-        
-        addSubview(actorsLabel)
-
-        actorsLabel.anchor(top: overviewText.bottomAnchor, left: leftAnchor, paddingTop: 10, paddingLeft: 10)
-        
-        addSubview(actorsList)
-        actorsList.anchor(top: actorsLabel.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 5, paddingLeft: 10, paddingRight: 10)
-
-        for (index, genre) in movie.genres.enumerated() {
-            if movie.genres.count == index + 1 {
-                genresLabel.text?.append("\(genre.name)")
-            } else {
-                genresLabel.text?.append("\(genre.name), ")
-            }
-        }
-        
-        for (index, actor) in movie.actors.enumerated() {
-            if movie.actors.count == index + 1 {
-                actorsList.text?.append("\(actor.name)")
-            } else {
-                actorsList.text?.append("\(actor.name), ")
-            }
-        }
-                
+        configureUI()
+        configureMovieDataForDisplay(movie: movie)
+    
 //        addSubview(actorCollection)
 //
 //        actorCollection.anchor(top: overviewText.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 10)
@@ -168,7 +115,6 @@ class DetailsView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
     
     //MARK: - Actions
     
@@ -185,23 +131,61 @@ class DetailsView: UIView {
     
     //MARK: - Helpers
     
-    func createGenreLabel(genre: String) -> UILabel {
-        let label = UILabel()
+    func configureUI() {
         
-        label.layer.borderWidth = 2
-        label.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
-        label.text = genre
+        addSubview(posterImage)
+        posterImage.anchor(top: safeAreaLayoutGuide.topAnchor, left: leftAnchor, paddingTop: 15, paddingLeft: 15)
+        posterImage.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.45).isActive = true
+        posterImage.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 0.45 * 1.5).isActive = true
         
-        return label
+        addSubview(rating)
+        addSubview(releaseLabel)
+        addSubview(trailerButton)
+        
+        rating.anchor(top: safeAreaLayoutGuide.topAnchor, left: posterImage.rightAnchor, paddingTop: 18, paddingLeft: 10)
+        releaseLabel.anchor(top: rating.bottomAnchor, left: posterImage.rightAnchor, paddingTop: 4, paddingLeft: 10)
+        trailerButton.anchor(top: releaseLabel.bottomAnchor, left: posterImage.rightAnchor, right: rightAnchor, paddingTop: 8, paddingLeft: 10, paddingRight: 15)
+        
+        addSubview(genresLabel)
+        genresLabel.anchor(top: trailerButton.bottomAnchor, left: posterImage.rightAnchor, right: rightAnchor, paddingTop: 5, paddingLeft: 10, paddingRight: 5)
+
+        addSubview(titleLabel)
+        addSubview(overviewText)
+        
+        titleLabel.anchor(top: posterImage.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 10, paddingLeft: 20, paddingRight: 20)
+        overviewText.anchor(top: titleLabel.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 10, paddingLeft: 20, paddingRight: 20)
+        
+        addSubview(actorsLabel)
+        actorsLabel.anchor(top: overviewText.bottomAnchor, left: leftAnchor, paddingTop: 10, paddingLeft: 10)
+        
+        addSubview(actorsList)
+        actorsList.anchor(top: actorsLabel.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 5, paddingLeft: 10, paddingRight: 10)
+        
     }
     
-    func createRatingView() -> UIView{
-        let ratingView = UIView()
-        let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 18)
-        label.text = "\(movie.rating)/10"
+    
+    func configureMovieDataForDisplay(movie: Movie) {
+        posterImage.sd_setImage(with: movie.posterPath)
+        titleLabel.text = movie.title
+        rating.setTitle("  \(movie.rating)/10", for: .normal)
+        releaseLabel.text = "\(movie.released)"
+        overviewText.text = movie.overview
         
-        return ratingView
+        for (index, genre) in movie.genres.enumerated() {
+            if movie.genres.count == index + 1 {
+                genresLabel.text?.append("\(genre.name)")
+            } else {
+                genresLabel.text?.append("\(genre.name), ")
+            }
+        }
+        
+        for (index, actor) in movie.actors.enumerated() {
+            if movie.actors.count == index + 1 {
+                actorsList.text?.append("\(actor.name)")
+            } else {
+                actorsList.text?.append("\(actor.name), ")
+            }
+        }
     }
     
 }
