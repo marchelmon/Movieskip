045e9d9070bef1372e7f5246e0ec796ecdff6c54
Marc Hulterström <marc.hulterstrom@gmail.com>
2021-04-15 19:44:54 +0200

    Creating MovieskipController

---
+++ Movieskip/Movieskip/Controller/MovieskipController.swift
@@ -0,0 +1,21 @@
+//
+//  MovieskipController.swift
+//  Movieskip
+//
+//  Created by marchelmon on 2021-04-15.
+//
+
+import UIKit
+import Firebase
+import GoogleSignIn
+import FBSDKCoreKit
+
+class MovieskipController: UIViewController {
+    
+    private let sceneDelegate = UIApplication.shared.connectedScenes.first!.delegate as! SceneDelegate
+    
+    
+    
+    
+    
+}
